5f53b4b16d1d078062e5ff760bc7f22e839d2ecf
pocorall <pocorall@gmail.com>
2013-08-18 21:05:29 +0900

    avoid feature warning

--- src/org/scaloid/hello/HelloScaloid.scala
+++ src/org/scaloid/hello/HelloScaloid.scala
@@ -9,16 +9,16 @@ class HelloScaloid extends SActivity {
     contentView = new SVerticalLayout {
       style {
         case b: SButton => b.textColor(Color.RED).onClick(toast("Bang!"))
-        case t: STextView => t.textSize(10 dip)
+        case t: STextView => t textSize 10.dip
         case v => v.backgroundColor(Color.YELLOW)
       }
 
       STextView("I am 10 dip tall")
       STextView("Me too")
-      STextView("I am taller than you").textSize(15 dip) // overriding
+      STextView("I am taller than you") textSize 15.dip // overriding
       SEditText("Yellow input field")
       SButton(R.string.red)
-    }.padding(20 dip)
+    } padding 20.dip
   }
 
 }
